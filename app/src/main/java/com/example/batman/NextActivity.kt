67af1f30d18c040a4bfc0513d86e097fd1caefe7
Sadi <banglaenglish477@gmail.com>
2023-03-12 23:39:50 -0700

    New Update

--- app/src/main/java/com/example/batman/NextActivity.kt
+++ app/src/main/java/com/example/batman/NextActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class NextActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //New Update
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_next)
     }
