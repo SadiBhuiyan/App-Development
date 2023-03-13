57f1595013f0a63704854f76f6f5ca9da8613911
Sadi <banglaenglish477@gmail.com>
2023-03-12 23:42:48 -0700

    New Update

--- app/src/main/java/com/example/batman/NextActivity.kt
+++ app/src/main/java/com/example/batman/NextActivity.kt
@@ -2,8 +2,10 @@ package com.example.batman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.batman.databinding.ActivityMainBinding
 
 class NextActivity : AppCompatActivity() {
+    ActivityMainBinding:binding
     override fun onCreate(savedInstanceState: Bundle?) {
         //New Update
         super.onCreate(savedInstanceState)
